091e17608d27146cd20c0bce49b1550bd511a73e
CraftingNetworkNL <83950417+CraftingNetworkNL@users.noreply.github.com>
2021-05-10 11:52:17 +0200

    init
    dd

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODMxNjEyNjQ3ODI5MDEyNTQw.YHXxoQ.tBe5duH_GPBjw_EA3x2RenuQ5KQ',
+        prefix: '=',
+        activity: 'crafting',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
